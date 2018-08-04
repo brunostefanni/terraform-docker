d2b66ecaf4d0d82d8fd68146c31a95d0df2668bc
Empreendimentos Pague Menos <PagueMenos@MacBook-Pro-de-Empreendimentos.local>
2018-08-04 06:48:23 -0300

    alterando index incluir equipe

--- servidor_apache/vars.tf
+++ servidor_apache/vars.tf
@@ -1,11 +1,16 @@
 variable "port_apache" {
   description = "Porta que sera usada nos servidores tomcat"
-  default     = 80
+  default     = 9090
 }
 
 variable "port_apache_2_externa" {
   description = "Porta que sera usada nos servidores tomcat"
-  default     = 80
+  default     = 8081
+}
+
+variable "port_apache_1_externa" {
+  description = "Porta que sera usada nos servidores tomcat"
+  default     = 8080
 }
 
 variable "path_arquivo_index" {
@@ -17,5 +22,6 @@ variable "path_arquivo_index" {
 variable "host_docker" {
   type        = "string"
   description = "host do docker"
-  default     = "tcp://127.0.0.1:2375/"
+  #default     = "tcp://127.0.0.1:2375/"
+  default = "unix:///var/run/docker.sock"
 }
